9e34e1eadb5520ace3e7f2f24bd68df0b22c5b57
notShal <shaldanillo@gmail.com>
2024-02-17 15:42:54 -0300

    editModeBtn

--- js/scripts.js
+++ js/scripts.js
@@ -2,24 +2,62 @@ const $modal = document.getElementById('modal');
 const $descriptionInput = document.getElementById('description');
 const $priorityInput = document.getElementById('priority');
 const $deadLineInput = document.getElementById('deadLine');
+const $idInput = document.getElementById('idInput');
 
-const $todoColumnBody = document.querySelector('#todoColumn .body')
+const $todoColumnBody = document.querySelector('#todoColumn .body');
+
+const $creationModeTitle = document.getElementById('creationModeTitle');
+const $editModeTitle = document.getElementById('editModeTitle');
+
+const $creationModeBtn = document.getElementById('creationModeBtn');
+const $editModeBtn = document.getElementById('editModeBtn');
 
 var todoList = [];
 
-function openModal() {
+function openModal(id) {
     $modal.style.display = "flex";
+
+    if (id) {
+        $creationModeTitle.style.display = "none";
+        $creationModeBtn.style.display = "none";
+
+        $editModeTitle.style.display = "block";
+        $editModeBtn.style.display = "block";
+
+        const index = todoList.findIndex(function (task) {
+            return task.id == id;
+        });
+
+        const task = todoList[index];
+
+        $idInput.value = task.id;
+        $descriptionInput.value = task.description;
+        $priorityInput.value = task.priority;
+        $deadLineInput.value = task.deadLine;
+    } else {
+        $creationModeTitle.style.display = "block";
+        $creationModeBtn.style.display = "block";
+
+        $editModeTitle.style.display = "none";
+        $editModeBtn.style.display = "none";
+
+    }
 }
 
 function closeModal() {
     $modal.style.display = "none";
+
+    $idInput.value = "";
+    $descriptionInput.value = "";
+    $priorityInput.value = "";
+    $deadLineInput.value = "";
 }
 
 function generateCards() {
     const todoListHtml = todoList.map(function (task) {
         const formattedDate = moment(task.deadLine).format('DD/MM/YYYY');
         return `
-            <div class="card">
+            <div class="card" ondblclick="openModal(${task.id})">
                 <div class="info">
                     <b>Descrição:</b>
                     <span>${task.description}</span>
@@ -44,6 +82,7 @@ function generateCards() {
 function createTask() {
 
     const newTask = {
+        id: Math.floor(Math.random() * 9999999),
         description: $descriptionInput.value,
         priority: $priorityInput.value,
         deadLine: $deadLineInput.value,
