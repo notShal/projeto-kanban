1c190948df7aed61ee4496eaccc23f1dd28824ac
notShal <shaldanillo@gmail.com>
2024-02-20 17:06:46 -0300

    local storage

--- js/scripts.js
+++ js/scripts.js
@@ -1,155 +1,184 @@
 const $modal = document.getElementById('modal');
 const $descriptionInput = document.getElementById('description');
 const $priorityInput = document.getElementById('priority');
-const $deadLineInput = document.getElementById('deadLine');
+const $deadlineInput = document.getElementById('deadline');
 const $columnInput = document.getElementById('column');
-const $idInput = document.getElementById('idInput');
+const $idInput = document.getElementById("idInput");
 
 const $creationModeTitle = document.getElementById('creationModeTitle');
-const $editModeTitle = document.getElementById('editModeTitle');
+const $editingModeTitle = document.getElementById('editingModeTitle');
 
 const $creationModeBtn = document.getElementById('creationModeBtn');
-const $editModeBtn = document.getElementById('editModeBtn');
+const $editingModeBtn = document.getElementById('editingModeBtn');
 
-var taskList = [];
+var tasks = localStorage.getItem("tasks");
+
+var taskList = tasks ? JSON.parse(tasks) : [];
+
+generateCards();
 
 function openModal(data_column) {
-    $modal.style.display = "flex";
+  $modal.style.display = "flex";
 
-    $columnInput.value = data_column;
+  $columnInput.value = data_column;
 
-    $creationModeTitle.style.display = "block";
-    $creationModeBtn.style.display = "block";
+  $creationModeTitle.style.display = "block";
+  $creationModeBtn.style.display = "block";
 
-    $editModeTitle.style.display = "none";
-    $editModeBtn.style.display = "none";
-
+  $editingModeTitle.style.display = "none";
+  $editingModeBtn.style.display = "none";
 }
 
 function openModalToEdit(id) {
-    $modal.style.display = "flex";
+  $modal.style.display = "flex";
 
-    $creationModeTitle.style.display = "none";
-    $creationModeBtn.style.display = "none";
+  $creationModeTitle.style.display = "none";
+  $creationModeBtn.style.display = "none";
 
-    $editModeTitle.style.display = "block";
-    $editModeBtn.style.display = "block";
+  $editingModeTitle.style.display = "block";
+  $editingModeBtn.style.display = "block";
 
-    const index = taskList.findIndex(function (task) {
-        return task.id == id;
-    });
+  const index = taskList.findIndex(function(task) {
+    return task.id == id;
+  });
 
-    const task = todoList[index];
+  const task = taskList[index];
 
-    $idInput.value = task.id;
-    $descriptionInput.value = task.description;
-    $priorityInput.value = task.priority;
-    $deadLineInput.value = task.deadLine;
-    $columnInput.value = task.column;
+  $idInput.value = task.id;
+  $descriptionInput.value = task.description;
+  $priorityInput.value = task.priority;
+  $deadlineInput.value = task.deadline;
+  $columnInput.value = task.column;
 }
 
 function closeModal() {
-    $modal.style.display = "none";
+  $modal.style.display = "none";
 
-    $idInput.value = "";
-    $descriptionInput.value = "";
-    $priorityInput.value = "";
-    $deadLineInput.value = "";
-    $columnInput.value = "";
+  $idInput.value = "";
+  $descriptionInput.value = "";
+  $priorityInput.value = "";
+  $deadlineInput.value = "";
+  $columnInput.value = "";
 }
 
 function resetColumns() {
-    document.querySelector('[data-column="1"] .body .cards_list').innerHTML = '';
-    document.querySelector('[data-column="2"] .body .cards_list').innerHTML = '';
-    document.querySelector('[data-column="3"] .body .cards_list').innerHTML = '';
-    document.querySelector('[data-column="4"] .body .cards_list').innerHTML = '';
-
+  document.querySelector('[data-column="1"] .body .cards_list').innerHTML = '';
+  document.querySelector('[data-column="2"] .body .cards_list').innerHTML = '';
+  document.querySelector('[data-column="3"] .body .cards_list').innerHTML = '';
+  document.querySelector('[data-column="4"] .body .cards_list').innerHTML = '';
 }
 
 function generateCards() {
 
-    resetColumns();
+  resetColumns();
 
-    taskList.forEach(function (task) {
-        const formattedDate = moment(task.deadLine).format('DD/MM/YYYY');
+  taskList.forEach(function(task) {
+    const formattedDate = moment(task.deadline).format('DD/MM/YYYY');
 
-        const columnBody = document.querySelector(`[data-column="${task.column}"] .body .cards_list`);
+    const columnBody = document.querySelector(`[data-column="${task.column}"] .body .cards_list`);
 
-        const card = `
-            <div class="card" ondblclick="openModalToEdit(${task.id})" 
-            draggable="true"
-            ondragstart="dragstart_handler(event)">
-                <div class="info">
-                    <b>Descrição:</b>
-                    <span>${task.description}</span>
-                </div>
+    const card = `
+      <div
+        id="${task.id}"
+        class="card"
+        ondblclick="openModalToEdit(${task.id})"
+        draggable="true"
+        ondragstart="dragstart_handler(event)"
+      >
+        <div class="info">
+          <b>Descrição:</b>
+          <span>${task.description}</span>
+        </div>
 
-                <div class="info">
-                    <b>Prioridade:</b>
-                    <span>${task.priority}</span>
-                </div>
+        <div class="info">
+          <b>Prioridade:</b>
+          <span>${task.priority}</span>
+        </div>
 
-                <div class="info">
-                    <b>Prazo:</b>
-                    <span>${formattedDate}</span>
-                </div>
-            </div>
-        `;
+        <div class="info">
+          <b>Prazo:</b>
+          <span>${formattedDate}</span>
+        </div>
+      </div>
+    `;
 
-        columnBody.innerHTML += card;
-    });
+    columnBody.innerHTML += card;
+  });
 }
 
 function createTask() {
+  const newTask = {
+    id: Math.floor(Math.random() * 9999999),
+    description: $descriptionInput.value,
+    priority: $priorityInput.value,
+    deadline: $deadlineInput.value,
+    column: $columnInput.value,
+  }
 
-    const newTask = {
-        id: Math.floor(Math.random() * 9999999),
-        description: $descriptionInput.value,
-        priority: $priorityInput.value,
-        deadLine: $deadLineInput.value,
-        column: $columnInput.value,
-    }
+  taskList.push(newTask);
 
-    taskList.push(newTask);
+  localStorage.setItem("tasks", JSON.stringify(taskList));
 
-    closeModal();
-    generateCards();
+  closeModal();
+  generateCards();
 }
 
 function updateTask() {
-    const task = {
-        id: $idInput.value,
-        description: $descriptionInput.value,
-        priority: $priorityInput.value,
-        deadLine: $deadLineInput.value,
-        column: $columnInput.value,
-    }
+  const task = {
+    id: $idInput.value,
+    description: $descriptionInput.value,
+    priority: $priorityInput.value,
+    deadline: $deadlineInput.value,
+    column: $columnInput.value,
+  }
 
-    const index = todoList.findIndex(function (task) {
-        return task.id == $idInput.value;
+  const index = taskList.findIndex(function(task) {
+    return task.id == $idInput.value;
+  });
+
+  taskList[index] = task;
+
+  localStorage.setItem("tasks", JSON.stringify(taskList));
+
+  closeModal();
+  generateCards();
+}
+
+function changeColumn(task_id, column_id) {
+  if (task_id && column_id) {
+    taskList = taskList.map((task) => {
+      if (task_id != task.id) return task;
+  
+      return {
+        ...task,
+        column: column_id,
+      };
     });
+  }
 
-    todoList[index] = task;
+  localStorage.setItem("tasks", JSON.stringify(taskList));
 
-    closeModal();
-    generateCards();
+  generateCards();
 }
 
 function dragstart_handler(ev) {
-    console.log(ev);
-    ev.dataTransfer.setData("my_custom_data", ev.target.id);
-    ev.dataTransfer.effectAllowed = "move";
+  console.log(ev);
+
+  // Add the target element's id to the data transfer object
+  ev.dataTransfer.setData("my_custom_data", ev.target.id);
+  ev.dataTransfer.effectAllowed = "move";
 }
 
 function dragover_handler(ev) {
-    ev.preventDefault();
-    ev.dataTransfer.dropEffect = "move";
-  }
+  ev.preventDefault();
+  ev.dataTransfer.dropEffect = "move";
+}
+
+function drop_handler(ev) {
+  ev.preventDefault();
+  // Get the id of the target and add the moved element to the target's DOM
+  const task_id = ev.dataTransfer.getData("my_custom_data");
+  const column_id = ev.target.dataset.column;
   
-  function drop_handler(ev) {
-    ev.preventDefault();
-    const task_id = ev.dataTransfer.getData("my_custom_data");
-    const column_id = ev.target.dataset.column;
-    
-    changeColumn(task_id, column_id);
-  }
+  changeColumn(task_id, column_id);
+}
